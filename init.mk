3ab58c781766f99b8fe550d72c918f652b1dd986
Dmitry Kireev <dmitry@hazelops.com>
2020-05-25 09:33:56 +0200

    Create .infra

--- init.mk
+++ init.mk
@@ -14,7 +14,7 @@ init.bootstrap: icmk.install examples.simple
 icmk.install: $(INFRA_DIR)/icmk
 $(INFRA_DIR)/icmk:
 	@echo "Installing icmk from $(ICMK_VERSION)"
-	cd $(INFRA_DIR) && $(GIT) submodule add https://github.com/hazelops/icmk.git icmk
+	mkdir -p $(INFRA_DIR) && cd $(INFRA_DIR) && $(GIT) submodule add https://github.com/hazelops/icmk.git icmk
 	cd $(INFRA_DIR)/icmk && $(GIT) checkout $(ICMK_VERSION)
 	@rm -f $(TMPDIR)/icmk.mk && rm -f $(TMPDIR)/icmk.mk
 	@echo "Done!"
