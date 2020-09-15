bbe562dd1682deb88485a95909edf503a1306b32
igorkotof <igor.kotov@hazelops.com>
2020-09-15 22:27:08 +0300

    added --service

--- serverless/main.mk
+++ serverless/main.mk
@@ -14,7 +14,7 @@ NPM ?= $(DOCKER) run --rm --workdir=/app -v $(ROOT_DIR)/$(PROJECT_PATH):/app nod
 # Serverless CLI Reference
 ########################################################################################################################
 CMD_SLS_SERVICE_INSTALL = $(NPM) install --save-dev
-CMD_SLS_SERVICE_DEPLOY = $(SLS) deploy --config $(SLS_FILE) --verbose --region $(AWS_REGION) --env $(ENV) --profile $(AWS_PROFILE)
+CMD_SLS_SERVICE_DEPLOY = $(SLS) deploy --config $(SLS_FILE) --service $(SVC) --verbose --region $(AWS_REGION) --env $(ENV) --profile $(AWS_PROFILE)
 CMD_SLS_SERVICE_INVOKE = $(SLS) invoke --function $(SVC) --path $(EVENT_FILE) --stage $(ENV) --region $(AWS_REGION) --log
 CMD_SLS_SERVICE_DESTROY = $(SLS) remove --config $(SLS_FILE) --stage $(ENV) --region $(AWS_REGION)
 CMD_SLS_SERVICE_BUILD = cd $(ROOT_DIR)/$(PROJECT_PATH) && make
