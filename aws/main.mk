f8bcdc5409e35a3646269fe3354baa0763ccc295
igorkotof <igor.kotov@hazelops.com>
2020-11-05 23:47:40 +0300

    ICMK installation of Session Manager Plugin

--- aws/main.mk
+++ aws/main.mk
@@ -15,6 +15,27 @@ AWS ?= $(DOCKER) run -v $(HOME)/.aws/:/root/.aws -i amazon/aws-cli:2.0.40
 CMD_AWS_LOGS_TAIL = @$(AWS) logs tail --profile $(AWS_PROFILE) $(SERVICE_NAME) --follow
 CMD_AWS_EC2_IMPORT_KEY_PAIR = @$(AWS) ec2 import-key-pair  --key-name="$(EC2_KEY_PAIR_NAME)" --profile $(AWS_PROFILE) --public-key-material="$(SSH_PUBLIC_KEY_BASE64)"
 
+# Getting OS|Linux info
+OS_NAME ?= $(shell uname -s)
+OS_DISTRIB ?= $(shell awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//')
+LINUX_CPU_VENDOR ?= $(shell lscpu | grep "Vendor ID:")
+LINUX_ARCH ?= $(shell uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+ARCH ?= $(shell echo $$(if echo "$(LINUX_CPU_VENDOR)" | grep -Fqe "Intel"; then echo "$(LINUX_ARCH)bit"; else echo "arm$(LINUX_ARCH)"; fi))
+LINUX_DISTRIB_TEMP ?= $(shell echo $$([ "$(OS_DISTRIB)" = "Ubuntu" ] && echo "ubuntu" || echo "linux")) #> /dev/null
+LINUX_DISTRIB ?= $(shell echo $(LINUX_DISTRIB_TEMP) | xargs)
+LINUX_PACKAGE_EXT ?= $(shell echo $$([ "$(OS_DISTRIB)" = "Ubuntu" ] && echo ".deb" || echo ".rpm")) #> /dev/null
+# Download Session Manager cmds
+SSM_DOWNLOAD_FOR_MAC_OS ?= curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip" && unzip sessionmanager-bundle.zip
+SSM_DOWNLOAD_FOR_LINUX_OS ?= curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/$(LINUX_DISTRIB)_$(ARCH)/session-manager-plugin$(LINUX_PACKAGE_EXT)" -o "session-manager-plugin$(LINUX_PACKAGE_EXT)"
+SSM_DOWNLOAD ?= $(shell echo $$(if [ "$(OS_NAME)" = "Linux" ]; then echo "$(SSM_DOWNLOAD_FOR_LINUX_OS)"; else echo "$(SSM_DOWNLOAD_FOR_MAC_OS)"; fi))
+# Installation Session Manager cmds
+LINUX_INSTALLER ?= $(shell echo $$(if [ "$(OS_DISTRIB)" = "Ubuntu" ]; then echo "sudo dpkg -i"; else echo "sudo yum install -y"; fi))
+SSM_INSTALL_ON_MAC_OS ?= sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+SSM_INSTALL_ON_LINUX_OS ?= $(LINUX_INSTALLER) session-manager-plugin$(LINUX_PACKAGE_EXT)
+SSM_INSTALL ?= $(shell echo $$(if [ "$(OS_NAME)" = "Linux" ]; then echo "$(SSM_INSTALL_ON_LINUX_OS)"; else echo "$(SSM_INSTALL_ON_MAC_OS)"; fi))
+# Post-install Session Manager check
+SSM_POST_INSTALL_CHECK = $(shell session-manager-plugin2)
+
 # Tasks
 ########################################################################################################################
 aws.debug: ## Show environment information for debug purposes
@@ -31,7 +52,19 @@ aws.key-pair: aws.import-ssh-key
 aws.import-ssh-key:
 	$(CMD_AWS_EC2_IMPORT_KEY_PAIR)
 
-
+# Install AWS SSM Session Manager plugin
+ssm-plugin: ssm-plugin.download ssm-plugin.install ssm-plugin.check
+ssm-plugin.download:
+	@$(SSM_DOWNLOAD)
+ssm-plugin.install:
+	@$(SSM_INSTALL)
+ssm-plugin.check:
+ifeq (, $(SSM_POST_INSTALL_CHECK))
+	@echo "\033[31m[FAILED]\033[0m SSM Session Manager Plugin is not installed or incorrectly configured.\nPlease go to https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html and install manually"
+else
+	@echo "\n\033[32m[OK]\033[0m SSM Session Manager Plugin is installed."
+endif
+	
 # Dependencies
 ########################################################################################################################
 # TODO: Add validation for ability to connect to AWS
