2e348cb18867776601a2e1123dd7f616a927a5da
Dmitry Kireev <dmitry@hazelops.com>
2020-11-06 19:58:06 +0300

    Ensure not to do any update if there is no ICMK directory

--- init.mk
+++ init.mk
@@ -24,7 +24,7 @@ icmk.clean:
 	@rm -rf $(INFRA_DIR)/icmk && echo "Cleaning Done"
 
 icmk.update: ## Updates ICMK
-	cd $(INFRA_DIR)/icmk && $(GIT) fetch --all && $(GIT) reset $(ICMK_VERSION) --hard && $(GIT) checkout $(ICMK_VERSION)
+	@[ -d "$(INFRA_DIR)/icmk" ] && (cd $(INFRA_DIR)/icmk && $(GIT) fetch --all && $(GIT) reset $(ICMK_VERSION) --hard && $(GIT) checkout $(ICMK_VERSION)) || echo "No ICMK installed. Please install it first." && exit 1
 
 icmk.update-init: ## Updates ICMK with a remote init script
 	@echo Updating via new init from https://hzl.xyz/icmk
