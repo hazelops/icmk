624c1d148831a1af286a97cf0355ba48ab4d9a4d
igorkotof <igor.kotov@hazelops.com>
2021-01-09 23:21:37 +0300

    CMD_SERVICE_SECRETS_PULL

--- aws/secrets.mk
+++ aws/secrets.mk
@@ -1,5 +1,6 @@
 # Macroses
 ########################################################################################################################
+SERVICE_SECRETS_BACKUP_FILE ?= $(INFRA_DIR)/env/$(ENV)/secrets/$(SVC)_backup.json
 SERVICE_SECRETS_FILE = $(INFRA_DIR)/env/$(ENV)/secrets/$(SVC).json
 SERVICE_SECRETS = $(shell cat $(SERVICE_SECRETS_FILE) | $(JQ) -e -r '. | to_entries[] | .key' )
 
@@ -17,6 +18,14 @@ CMD_SERVICE_SECRETS_DELETE = @ (echo $(foreach item, $(shell aws ssm --profile=$
 		$(shell aws ssm --profile=$(AWS_PROFILE) delete-parameter --name $(item))) > /dev/null ) && \
 		echo "\033[32m[OK]\033[0m /$(ENV)/$(SVC)/* secrets deleted" || echo "\033[31m[ERROR]\033[0m /$(ENV)/$(SVC)/* secrets deletion"
 
+CMD_SERVICE_ALL_SECRETS_NAMES = $(foreach item, $(shell aws ssm --profile=$(AWS_PROFILE) get-parameters-by-path \
+		--path "/$(ENV)/$(SVC)" --recursive | $(JQ) -e -r '.Parameters[] | .Name' ), $(item))
+CMD_SERVICE_SECRETS_PULL = @ (echo $(shell echo "{\"INFO\":\"EMPTY_JSON_CREATED\"}" > $(SERVICE_SECRETS_BACKUP_FILE)) && \
+		$(foreach item, $(CMD_SERVICE_ALL_SECRETS_NAMES), \
+		$(shell $(JQ) --arg value "$(shell echo $(item) | sed 's|.*/||')" '.[$$value] = "'$(shell aws ssm --profile=$(AWS_PROFILE) get-parameter --name $(item) --query Parameter.Value)'"' \
+		$(SERVICE_SECRETS_BACKUP_FILE) > tmp.json && mv tmp.json $(SERVICE_SECRETS_BACKUP_FILE))) > /dev/null ) && \
+		$(JQ) 'del(."INFO")' $(SERVICE_SECRETS_BACKUP_FILE) > tmp.json && mv tmp.json $(SERVICE_SECRETS_BACKUP_FILE) && \
+		echo "\033[32m[OK]\033[0m /$(ENV)/$(SVC)/* secrets pulled" || echo "\033[31m[ERROR]\033[0m /$(ENV)/$(SVC)/* secrets getting"
 # Tasks
 ########################################################################################################################
 secrets.push:
