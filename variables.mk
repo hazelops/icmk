e0b684043a8d252293e4d5fad8e501d3e173c632
Dmitry Borisov <kobrikx@gmail.com>
2021-10-18 10:16:12 +0300

    Fixes1

--- variables.mk
+++ variables.mk
@@ -7,7 +7,6 @@ BUSYBOX_VERSION ?= 1.31.1
 # This is a workaround for syntax highlighters that break on a "Comment" symbol.
 HASHSIGN = \#
 SLASHSIGN = /
-ECS_DEPLOY_SHA ?= sha256:acca364f44b8cbc01401baf53a39324cd23c11257c3ab66ca52261f85e69f60d
 
 # For unit tests
 BATS_BIN_PATH = tests/test/libs/bats/bin
