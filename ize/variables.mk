4a45bf4d8ff238164e03d8f2517e7050b6500dc5
Dmitry Borisov <kobrikx@gmail.com>
2022-01-03 20:07:39 +0300

    Add ize installer from github releases

--- ize/variables.mk
+++ ize/variables.mk
@@ -1,7 +1,9 @@
 # This file should contain variables used in current module
 ##################################################################
 #### Default settings for IZE
+MACOS_BITS ?= $(shell uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 IZE_DIR ?= $(INFRA_DIR)/bin
-TMPDIR ?= "/tmp"
-IZE_VERSION ?= "v0.0.1"
-IZE_DOWNLOAD_URL = "https://hazelops-ize-bin.s3.amazonaws.com/hazelops/ize/releases/download/$(IZE_VERSION)/$(OS_NAME).zip"
+TMPDIR ?= /tmp
+IZE_VERSION ?= 0.1.0
+IZE_ARCHIVE_NAME = ize_$(IZE_VERSION)_$(OS_NAME)_$(ARCH).tar.gz
+IZE_DOWNLOAD_URL = https://github.com/hazelops/ize/releases/download/$(IZE_VERSION)/$(IZE_ARCHIVE_NAME)
