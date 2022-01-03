e8b633b5b0ccdcb1514ffcb94c2704ce5be29fd5
Dmitry Borisov <kobrikx@gmail.com>
2022-01-03 22:38:56 +0300

    Add ize installer from github releases 2

--- ize/variables.mk
+++ ize/variables.mk
@@ -1,9 +1,12 @@
 # This file should contain variables used in current module
 ##################################################################
 #### Default settings for IZE
-MACOS_BITS ?= $(shell uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+MACOS_ARCH_NAME = $(shell uname -m)
+MACOS_ARCH = $(shell echo $$(if [ "$(MACOS_ARCH_NAME)" = "x86_64" ]; then echo "amd64"; else echo "arm64"; fi))
 IZE_DIR ?= $(INFRA_DIR)/bin
 TMPDIR ?= /tmp
 IZE_VERSION ?= 0.1.0
-IZE_ARCHIVE_NAME = ize_$(IZE_VERSION)_$(OS_NAME)_$(ARCH).tar.gz
+MACOS_ARCHIVE_NAME = ize_$(IZE_VERSION)_$(OS_NAME)_$(MACOS_ARCH).tar.gz
+LINUX_ARCHIVE_NAME = ize_$(IZE_VERSION)_$(OS_NAME)_$(ARCH).tar.gz
+IZE_ARCHIVE_NAME ?= $(shell echo $$(if [ "$(OS_NAME)" = "Linux" ]; then echo "$(LINUX_ARCHIVE_NAME)"; else echo "$(MACOS_ARCHIVE_NAME)"; fi))
 IZE_DOWNLOAD_URL = https://github.com/hazelops/ize/releases/download/$(IZE_VERSION)/$(IZE_ARCHIVE_NAME)
